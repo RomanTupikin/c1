4b992a279f3fa2110028701ea23ec03cf6be0d09
Roman <oeni44@gmail.com>
2022-11-14 10:34:04 +0500

    Исправления

--- task05/Program.cs
+++ task05/Program.cs
@@ -8,9 +8,9 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n % 2 == 0)
 {
-    Console.WriteLine($"число {n} четное");
+    Console.WriteLine($"Число {n} четное");
 }
 else
 {
-    Console.WriteLine($"число {n} не четное");
+    Console.WriteLine($"Число {n} не четное");
 }
