50c2d0db7e18ff9fb4f6e8ffeb21fa130a76dd2e
Roman <oeni44@gmail.com>
2022-11-14 10:07:33 +0500

    Сделал дз

--- task02/Program.cs
+++ task02/Program.cs
@@ -5,3 +5,17 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
+
+Console.Write("Введите трехзначное число чтобы узнать третью цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number < 1000 && number > 99)
+{
+    number = number % 10;
+
+    Console.WriteLine($"Третья цифра {number}");
+}
+else
+{
+    Console.WriteLine("Трехзначное число пожалуйста");
+}
