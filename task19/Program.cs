08f3ce7b7f88786f0d8c126baab3ab1540a3407d
Roman <oeni44@gmail.com>
2022-11-28 20:13:28 +0500

    3 ДЗ

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = Math.Abs(number);
+if (number1 > 9999 && number1 < 100000)
+{
+    int num1 = number / 10000;
+    int num2 = number / 1000 - number / 10000 * 10;
+    int num4 = number / 10 - number / 100 * 10;
+    int num5 = number - number / 10 * 10;
+    if (num1 == num5 && num2 == num4)
+    {
+        Console.WriteLine($"{number} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} не палиндром");
+    }
+}
+else 
+{
+    Console.WriteLine("Неверное число");
+}
---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int cube = num * num * num;
+    Console.WriteLine($"{num, 4} {cube, 4}");
+}
+int count = 1;
+int num = Math.Abs(number);
+while (count <= num)
+{
+    Cube(count);
+    count++;
+}
